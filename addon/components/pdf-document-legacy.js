4ab5ff66c692c72feffd7b8e931ceedaa50f77ec
Kevin Jhangiani <kevin@soxhub.com>
2017-01-30 23:01:39 +0000

    change `pdf-document` to use `forEach` instead of `map`
    Ember 1.13.x introduces glimmer components, and handles setting arrays much more intelligently
    For pre-ember 1.13, re-setting an array causes massive penalities in re-rendering
    
    This commit does the following (which should have no adverse affects on later versions of ember)
    
    - initializes `pages` in `pdf-document` to Ember.A() (for users w/ prototype extensions disabled)
    - initializes `pages` in `_loadPages` to an Ember.A()
    - uses `set(this, 'pages', pages)` only once, in the `_loadPages` function
    - all other iterations over pages now use `forEach` instead of `map`, and do not overwrite the pages object with another set

--- addon/components/pdf-document-legacy.js
+++ addon/components/pdf-document-legacy.js
@@ -3,62 +3,31 @@
   This legacy component is for backwards compatibility with ember 1.12, and uses an actions block
 **/
 
-
-/**
-* This program has 3 basic workflows.
-*
-* First is the loading of the document and generation of the individual pages.
-* This happens using methods on the PDFJS global and is a promise chain. It
-* populates the get(this, 'pages') array and renders the initial pages.
-*
-* The second workflow is the sizing of the pages and blank pages. This happens
-* through communication with pdf-page. pdf-page lets pdf-document know the height
-* of the first canvas rendered. pdf-document observes this and then sets the
-* height property on all pages. All pdf-pages then resize themselves.
-*
-* The third workflows are the scroll and resize bindings. The addon will only
-* render 3-5 pages at a time. When the user scrolls new pages will become rendered
-* and old pages will be removed from the DOM. Resizing the width of the page will
-* cause the pages to be re-rendered and all the page sizes will change. It also
-* makes its best effort at snapping the page to what it was before resizing. This
-* can get a little wonky when resizing really small and huge.
-*
-* It also has testing hooks built in that will notify acceptance tests when certain
-* promises have been resolved. If it finds the #ember-testing-container then it will
-* activate these hooks.
-*/
-
 import Ember from 'ember';
 import layout from '../templates/components/pdf-document-legacy';
 import PDFDocumentComponent from './pdf-document';
 
-/**
-*  Test hooks so tests know when all the async calls in this component have
-*  been resolved.
-*/
-let testing = $('#ember-testing-container').length;
-
-let componentLoaded;
-let componentScrolled;
-let finishedLoading;
-let finishedScrolling;
-
-if (testing) {
-  componentLoaded = new Promise((resolve/*, reject*/) => {
-    finishedLoading = resolve;
-  });
-
-  componentScrolled = new Promise((resolve/*, reject*/) => {
-    finishedScrolling = resolve;
-  });
-}
+const {
+  get,
+  set
+} = Ember;
 
 export default PDFDocumentComponent.extend({
   layout,
 
+  setHeight: function(height, resize) {
+    if (get(this, 'pageHeight') !== height) {
+      set(this, 'pageHeight', height);
+    }
+
+    if (resize) {
+      set(this, 'resize', true);
+    }
+  },
+
   actions: {
-    setHeight: function() {
-      this.setHeight(...arguments);
+    setHeight: function(that, height, resize) {
+      this.setHeight(height, resize);
     },
 
     doneScrolling: function() {
